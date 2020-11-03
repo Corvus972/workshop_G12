0cdcaa7adb058727c5a31fdaacb0da30fb694417
Alex347 <Daily365!!!>
2020-11-03 15:50:55 +0100

    almost done

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -9,32 +9,61 @@ use App\Entity\User;
 use App\Entity\Category;
 use App\Entity\Product;
 use Faker;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+        $prod = [];
+        for ($j = 0; $j < 5; $j++) {
+            $cat = new Category();
+            $cat->setName("Categorie_".$j);
+            for ($i = 0; $i < 100; $i++) {
+                $prod[$i] = new Product();
+                $prod[$i]->setName("Product_".$i)
+                    ->setImage("https://picsum.photos/id/".$i."/200/300")
+                    ->setProductRef("")
+                    ->setQuantity(($i +1) * 4)
+                    ->setUnit("par 6")
+                    ->setCategory($cat);
+
+                $manager->persist($prod[$i]);
+            }
+            $manager->persist($cat);
+        }
+
         $test_password = 'test';
-        $factory = $this->container->get('security.encoder_factory');
+
+        $user = [];
         for ($count = 0; $count < 20; $count++) {
             $user = new User();
-            $user->setFirstName('Albert_'.$count);
-            $user->setLastName("Houbre_".$count);
-            $user->setEmail('admin'.$count.'@admin.com');
-            $user->setFirstname('Zozo_'.$count);
-            $user->setRoles(array('ROLE_ADMIN', 'ROLE_USER'));
-            $user->setAddressLine1("line one of the address");
-            $user->setAddressLine2("line two of the address");
-            $user->setZipCode("34000");
-            $user->setRegion("34");
-            $user->setCity("Montpellier");
-            $encoder = $factory->getEncoder($user);
-            $password = $encoder->encodePassword($test_password, $user->getSalt());
-            $user->setPassword($password);
+            $user->setFirstName('Albert_'.$count)
+                ->setLastName("Houbre_".$count)
+                ->setEmail('admin'.$count.'@admin.com')
+                ->setFirstname('Zozo_'.$count)
+                ->setRoles(array('ROLE_ADMIN', 'ROLE_USER'))
+                ->setAddressLine1("line one of the address")
+                ->setAddressLine2("line two of the address")
+                ->setZipCode("34000")
+                ->setRegion("34")
+                ->setCity("Montpellier")
+    //            $encoded = $encoder->encodePassword($user, $test_password);
+    //            ->setPassword($encoded);
+                ->setPassword($test_password);
+
+            foreach ($prod as $key => $val ){
+                $user -> addProduct($prod[$key]);
+            }
 
             $manager->persist($user);
         }
+
         $manager->flush();
+
+
+
+
 
 
 
