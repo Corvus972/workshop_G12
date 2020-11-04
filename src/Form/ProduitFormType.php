d948332b88274e21e49a5297c1c8f9b5f0d7ed3a
bryan.mediabat <bryan.mediabat@gmail.com>
2020-11-04 18:23:25 +0100

    Add produit and delete working

---
+++ src/Form/ProduitFormType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Product;
+use App\Entity\Category;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
+class ProduitFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('unit',ChoiceType::class, [
+                    'choices'  => [
+                        'Kg' => 'kg',
+                        'Litre' => 'litre',
+                        'Unité' => 'unite',
+                        'Autre'=>'autre'
+                              ],
+                    ])
+            ->add('quantity')
+            ->add('image',FileType::class)
+            ->add('price')
+            ->add('category',EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => function($Category){ // function qui recupere les infos de l'utilisateur
+                    return $Category->getName();
+                }
+            ])
+        ;
+    }
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Product::class,
+        ]);
+    }
+}
