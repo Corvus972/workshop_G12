136a1f7f897f8d05b271fde21dec66c9121ccc7a
Alex347 <Daily365!!!>
2020-11-04 17:24:52 +0100

    products done!

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Product;
+use App\Repository\OrderRepository;
+use App\Repository\PackRepository;
 use App\Repository\ProductRepository;
 use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
@@ -17,24 +19,30 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Vicopo\Vicopo;
 
-
+/**
+ * @Route("/product", name="product")
+ */
 class ProductController extends AbstractController
 {
     /**
-     * @Route("/product", name="product")
+     * @Route("/index", name="product_index")
      * @param ProductRepository $productRepository
      * @param Request $request
+     * @param UserInterface $userProfile
+     * @param UserRepository $userRepository
+     * @param PackRepository $packRepository
      * @return Response
      * @throws Exception
      */
 
-    public function index(ProductRepository $productRepository, Request $request, UserInterface $userProfile, UserRepository $userRepository): Response
+    public function index(ProductRepository $productRepository, Request $request, UserInterface $userProfile, UserRepository $userRepository, PackRepository $packRepository): Response
     {
-
-//        $userProfile -> getRegion();
         $req = $request->query->get('q');
         $userRegion = $userProfile -> getRegion();
         $farmerInfo = $userRepository -> findBy(['region' => $userRegion]);
+
+        $packFilterByRegion = $packRepository -> findBy((['id' => $farmerInfo]));
+
         $productsFilterByRegion = $productRepository -> findBy((['user' => $farmerInfo]));
         $userRegion = Vicopo::https('34');
 //        dump($userRegion);
@@ -47,7 +55,32 @@ class ProductController extends AbstractController
         }
 
         return $this->render('product/index.html.twig', [
+            'packs' => $packFilterByRegion,
             'products' => $catalogue,
+        ]);
+    }
+
+    /**
+     * @Route("/recipe", name="product_recipe")
+     * @param OrderRepository $orderRepository
+     * @param Request $request
+     * @param ProductRepository $productRepository
+     * @return Response
+     */
+
+    public function recipe(OrderRepository $orderRepository, Request $request, ProductRepository $productRepository): Response
+    {
+
+//        $userProfile -> getRegion();
+        $req = $request->query->get('q');
+        $recipes = $orderRepository -> findOneBy(['id' => $req]);
+
+        dump($recipes);
+        die;
+
+
+        return $this->render('product/recipe.html.twig', [
+            'recipes' => $recipes,
         ]);
     }
 }
