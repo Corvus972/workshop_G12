da3699f8269e38e6148dad5af124f8720b911ae3
stanleyHarry <jesoph.stanley@hotmail.com>
2020-11-04 08:44:04 +0100

    add cart controller

--- src/Controller/CartProducerController.php
+++ src/Controller/CartProducerController.php
@@ -3,15 +3,19 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class CartProducerController extends AbstractController
 {
     /**
      * @Route("/cart/producer", name="cart_producer")
+     * @param Request $request
+     * @param UserInterface $user
      */
-    public function index(): Response
+    public function cartProducer(Request $request, UserInterface $userProfile): Response
     {
         return $this->render('cart_producer/index.html.twig', [
             'controller_name' => 'CartProducerController',
